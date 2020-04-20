088534ae2a92890706a55df64f23aa98e5a3b97a
name <email@email.com>
2020-04-19 19:50:09 -0500

    star rating works when it comes first in the form

--- src/pages/reviewComponent.jsx
+++ src/pages/reviewComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import StarRatings from 'react-star-ratings';
 
 export default class ReviewComponent extends React.Component {
     constructor(props){
@@ -10,10 +11,12 @@ export default class ReviewComponent extends React.Component {
             restroomId: this.props.match.params.id,
             businessName: '',
             username: '',
-        //     rating: 0,
-            reviewText: ''
+            rating: 1,
+            reviewText: '',
+            message: null
           }
         
+        this.changeRating = this.changeRating.bind(this);
         this.loadData = this.loadData.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         // this.validate = this.validate.bind(this);
@@ -33,12 +36,32 @@ export default class ReviewComponent extends React.Component {
         console.log(this.state.restroomId);
     }
 
-    onSubmit (){
-        console.log('submitted');
+    changeRating(newRating, name ) {
+        this.setState({
+          rating: newRating
+        });
+     
+      }
+    
+
+    onSubmit (values){
+        const review = {
+            restroomId: this.state.restroomId,
+            businessName: this.state.businessName,
+            username: values.username,
+            rating: values.rating,
+            reviewText: values.reviewText
+          };
+
+          console.log(review);
+        // axios.post(`http://localhost:8080/restrooms/${this.state.restroomId}/details`, review)
+        //   .then((response) => {
+        //       this.setState({message: `Review added to Restroom ID: ${this.state.restroomId}.`});
+        //   })
         
     }
     render() {
-        let {restroomId, businessName, username, rating, reviewText} = this.state;
+        const {restroomId, businessName, username, rating, reviewText} = this.state;
         if (this.state.restroomId === -1) {
             return
         }
@@ -48,7 +71,8 @@ export default class ReviewComponent extends React.Component {
                 { this.state.isLoading && <div>Loading...please wait!</div>}
           
             {!this.state.isLoading && <div>
-                <h1>Restroom Details</h1>
+                <h1>Add Review</h1>
+                {this.state.message && <div className="alert alert-success">{this.state.message}</div>}
                 <div className="container">
                     <Formik
                         initialValues = {{restroomId, businessName, username, rating, reviewText}}
@@ -63,22 +87,38 @@ export default class ReviewComponent extends React.Component {
                     {// <ErrorMessage name="userName" component="div"
                     //   className="alert alert-warning" />
                 }
+                
                     <fieldset className="form-group">
                       <label>Restroom ID: </label>
                       <Field className="form-control" type="text" name="id" value={restroomId} disabled />
                     </fieldset>
+
                     <fieldset className="form-group">
                       <label>Business Name: </label>
                       <Field className="form-control" type="text" name="businessName" value={businessName} disabled />
                     </fieldset>
+
+                    <fieldset className="form-group">
+                    <label>Rating: </label>
+                    <StarRatings
+                      rating={this.state.rating}
+                      starRatedColor="blue"
+                      changeRating={this.changeRating}
+                      numberOfStars={5}
+                      name='rating'
+                  />
+                  </fieldset>
+
                     <fieldset className="form-group">
                       <label>Username: </label>
                       <Field className="form-control" type="text" name="username" />
                     </fieldset>
+                  
                     <fieldset className="form-group">
                       <label>Review: </label>
                       <Field className="form-control" type="textarea" name="reviewText" />
                     </fieldset>
+
                     <button className="btn btn-success" type="submit">Save</button>
                     <a href='/restrooms/'>Cancel</a>
                 </Form>
